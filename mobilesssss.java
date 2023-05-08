619e76ab17ac28dab0604e888fb447b68173f1b9
CHENTHILSP <132836556+CHENTHILSP@users.noreply.github.com>
2023-05-08 21:42:41 +0530

    Add files via upload

---
+++ mobilesssss.java
@@ -0,0 +1,27 @@
+package classday2;
+
+public class mobilesssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName="Fossil";
+		System.out.println(mobileBrandName);
+		char mobileLogo='F';
+		System.out.println(mobileLogo);
+		short noofmobilepiece=5;
+		System.out.println(noofmobilepiece);
+		int modelnumber=15;
+		System.out.println(modelnumber);
+		long mobileimeinumber=3452348L;
+		System.out.println(mobileimeinumber);
+		float mobileprice=125000;
+		System.out.println(mobileprice);
+		boolean isdamaged=false;
+		System.out.println(isdamaged);
+		
+		
+		
+
+	}
+
+}
---
+++ twowheeler.java
@@ -0,0 +1,25 @@
+package classday2;
+
+public class twowheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noofwheels = 2;
+		System.out.println(noofwheels);
+		short noofmirrors = 2;
+		System.out.println(noofmirrors);
+		long chassisnumber = 75643542L;
+		System.out.println(chassisnumber);
+		boolean ispunctured = true;
+		System.out.println(ispunctured);
+		String bikename="bmw";
+		System.out.println(bikename);
+		double runningKM = 456.13d;
+		System.out.println(runningKM);
+		
+		
+		
+
+	}
+
+}
